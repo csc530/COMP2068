22902d53438f614d55e75ccc00fdbcca8787f105
csc530 <cscousin@lakeheadu.ca>
2022-01-19 21:02:58 -0500

    Added use of colours in CLI

--- Lab 1-Rock-Paper-Scissors/game.js
+++ Lab 1-Rock-Paper-Scissors/game.js
@@ -1,6 +1,18 @@
 const prompt = require('prompt');
 const colours = require('colors/safe');
 prompt.start();
+colours.enable();
+
+colours.setTheme({
+    userInput: ['green', 'italic'],
+    error: 'red',
+    warning: ['yellow', 'trap'],
+    prompt: ['blue', 'bold', ]
+});
+const cUi = colours.userInput;
+const cError = colours.error;
+const cWarning = colours.warninig;
+const cPrompt = colours.prompt;
 prompt.message = '';
 prompt.delimiter = "";
 prompt.colors = true;
@@ -8,13 +20,14 @@ prompt.colors = true;
 let required = {
     'name': 'name',
     required: true,
-    message: "Please could you tell me you're name it would really make my day :)",
+    message: colours.error("Please could you tell me you're name it would really make my day :)"),
     allowEmpty: false,
     type: 'string',
-    description: "User's name"
+    description: colours.prompt("User's name: "),
 };
 prompt.get(required, (err, result) => {
     if(err)
         console.error(`Something has gone wrong.\n${err.name}: ${err.message}\nStack Trace: \n\n${err.stack}`);
-    process.stdout.write(`So your name is ${result.name}.\nHi ${result.name} nice to meet you would you like to play rock paper scissors with me?`);
+    process.stdout.write(`So your name is ${cUi(result.name)}.\nHi ${cUi(result.name)} nice to meet you would you like to play rock paper scissors with me?\n`);
 });
+
