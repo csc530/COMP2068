b7cf9894d5bab8e2af985d0c41685bdf904e92a8
csc530 <cscousin@lakeheadu.ca>
2022-01-19 20:45:07 -0500

    Added more control on the prompt to get user's name

--- Lab 1-Rock-Paper-Scissors/game.js
+++ Lab 1-Rock-Paper-Scissors/game.js
@@ -1,12 +1,20 @@
-let prompt = require('prompt');
+const prompt = require('prompt');
+const colours = require('colors/safe');
 prompt.start();
-console.dir(prompt.start);
 prompt.message = '';
 prompt.delimiter = "";
 prompt.colors = true;
 
-prompt.get({'name': 'name', required: true},(err,result)=>{
+let required = {
+    'name': 'name',
+    required: true,
+    message: "Please could you tell me you're name it would really make my day :)",
+    allowEmpty: false,
+    type: 'string',
+    description: "User's name"
+};
+prompt.get(required, (err, result) => {
     if(err)
-    console.error(`Something has gone wrong.\n${err.name}: ${err.message}\nStack Trace: \n\n${err.stack}`);
+        console.error(`Something has gone wrong.\n${err.name}: ${err.message}\nStack Trace: \n\n${err.stack}`);
     process.stdout.write(`So your name is ${result.name}.\nHi ${result.name} nice to meet you would you like to play rock paper scissors with me?`);
 });
