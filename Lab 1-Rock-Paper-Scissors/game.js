20c5df99867f527d0ef43925248cc68baf31d0af
csc530 <77406318+csc530@users.noreply.github.com>
2022-01-23 16:04:33 -0500

    Configured an eslint for this project

--- Lab 1-Rock-Paper-Scissors/game.js
+++ Lab 1-Rock-Paper-Scissors/game.js
@@ -1,61 +1,63 @@
-const prompt = require('prompt');
-const colours = require('colors/safe');
+const prompt = require("prompt");
+const colours = require("colors/safe");
 prompt.start();
 colours.enable();
 
 colours.setTheme({
-    userInput: ['green', 'italic'],
-    error: 'red',
-    warning: ['yellow', 'trap'],
-    prompt: ['blue', 'bold',]
+	userInput: ["green", "italic"],
+	error: "red",
+	warning: ["yellow", "trap"],
+	prompt: ["blue", "bold",]
 });
-const cUi = colours.userInput;
-const cError = colours.error;
-const cWarning = colours.warninig;
-const cPrompt = colours.prompt;
-prompt.message = '';
+const colour = {
+	ui: colours.error,
+	warning: colours.warninig,
+	prompt: colours.prompt,
+};
+prompt.message = "";
 prompt.delimiter = "";
 prompt.colors = true;
 
 let required = {
-    'name': 'name',
-    required: true,
-    message: colours.error("Please could you tell me you're name it would really make my day :)"),
-    allowEmpty: false,
-    type: 'string',
-    description: colours.prompt("User's name: "),
+	"name": "name",
+	required: true,
+	message: colours.error("Please could you tell me you're name it would really make my day :)"),
+	allowEmpty: false,
+	type: "string",
+	description: colours.prompt("User's name: "),
 };
 function getUserName() {
-    prompt.get(required, (err, result) => {
-        checkForErrors(err);
-        process.stdout.write(`So your name is ${cUi(result.name)}.\nHi ${cUi(result.name)} nice to meet you would you like to play rock paper scissors with me?\n`);
-    });
+	prompt.get(required, (err, result) => {
+		checkForErrors(err);
+		process.stdout.write(`So your name is ${colour.ui(result.name)}.\nHi ${colour.ui(result.name)} nice to meet you would you like to play rock paper scissors with me?\n`);
+	});
 }
 function checkForErrors(err) {
-    if(err)
-        console.error(`Something has gone wrong.\n${err.name}: ${err.message}\nStack Trace: \n\n${err.stack}`);
-    process.exit(1);
+	if (err)
+		console.error(`Something has gone wrong.\n${err.name}: ${err.message}\nStack Trace: \n\n${err.stack}`);
+	process.exit(1);
 }
 
 function playRPS() {
-    prompt.get('play', (err, result) => {
-        checkForErrors(err);
-        if(result)
-        console.log("great");
-    });
+	prompt.get("play", (err, result) => {
+		checkForErrors(err);
+		if (result)
+			console.log("great");
+	});
 }
 
 function explainRules() {
-    console.log(rules);
-    prompt.get(start,)
+	console.log(rules);
+	prompt.get(start);
 }
 
-function play(){
+function play() {
 
 }
 
 function main() {
-    getUserName();
+	getUserName();
 }
 
-//main();
+//Starts the rock paper scissors game
+main();
