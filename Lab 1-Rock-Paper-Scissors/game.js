ffd39269bb338042d7a099f157c94e00755e0205
csc530 <cscousin@lakeheadu.ca>
2022-01-25 10:12:56 -0500

    feat(main): :sparkles: Added logic to determine a winner
    Implemented the winner function to see who won the game of rock paper scissors

--- Lab 1-Rock-Paper-Scissors/game.js
+++ Lab 1-Rock-Paper-Scissors/game.js
@@ -25,8 +25,10 @@ const themes = {
 	prompt: colours.prompt,
 };
 
-function checkForErrors(err) {
-	if (err) {
+function checkForErrors(err)
+{
+	if (err)
+	{
 		console.log(themes.err(`Something has gone wrong.\n${err.name}: ${err.message}\nStack Trace:\n${err.stack}`));
 		process.exit(1);
 	}
@@ -34,18 +36,52 @@ function checkForErrors(err) {
 
 
 /**
- * @param {string} pc 
- * @param {string} user 
+ *  * A function to dertermine who won given 2 arguments of rock, paper, or scissors
+ * @param {string} pc The pc's choice
+ * @param {string} user The user's choice
  */
-function winner(pc, user){
-
+function winner(pc, user)
+{
+	pc = pc[0].toLowerCase();
+	user = user[0].toLowerCase();
+	if (pc === user)
+		console.log(themes.ui("It's a tie!👔"));
+	else
+	{
+		switch (pc)
+		{
+		case 'r':
+			if(user === 's')
+				console.log('Haha, I win!');
+			else
+				console.log('Darn, you beat me!\nYou win!🏅');
+			break;
+		case 's':
+			if(user === 'r')
+				console.log('Wow! You won!\nCongratulations!🎉');
+			else
+				console.log('😎\nLooks like I beat you!\nI won!');
+			break;
+		case 'p':
+			if(user ==='s')
+				console.log('Amazing! You won!🌟');
+			else
+				console.log('Hehehe, winner winner.\nI\'m the winner!🥳');
+			break;
+		default:
+			console.error('Oops! Looks like something went wrong');
+			break;
+		}
+	}
+	return '';
 }
 /**
  * This will execute the game of rock-paper-scissors
  * It will get the user's choice and compare it to the computer's choice
  * and then a winner will be decided if there is one
  */
-function play() {
+function play()
+{
 	const query = {
 		name: 'choice',
 		allowEmpty: false,
@@ -69,14 +105,16 @@ function play() {
 		],
 	};
 	const pcChoice = randomRPS();
-	prompt.get(query, (err, result) => {
+	prompt.get(query, (err, result) =>
+	{
 		checkForErrors(err);
-		console.log(themes.ui(`I choose ${pcChoice}\n`));	
-		return winner(pcChoice);
+		console.log(themes.ui(`I choose ${pcChoice}\n`));
+		return winner(result.choice, pcChoice);
 	});
 }
 
-function explainRules() {
+function explainRules()
+{
 	const query = {
 		name: 'play', description: themes.prompt('Would you like to play rock-paper-scissors? '),
 		message: themes.warning('Please enter (Y)es or (N)o'),
@@ -88,9 +126,11 @@ function explainRules() {
 		'Scissors beats paper\n' +
 		'and\n' +
 		'Paper beats rock';
-	prompt.get(query, (err, result) => {
+	prompt.get(query, (err, result) =>
+	{
 		checkForErrors(err);
-		if (result['play'][0] === 'y') {
+		if (result['play'][0] === 'y')
+		{
 			console.log(rules + '\n--------------------------------\n');
 			return play();
 		}
@@ -104,7 +144,8 @@ function explainRules() {
  * A function to randomly pick rock paper or scissors
  * @returns a string of either rock, paper, or scissors
  */
-function randomRPS() {
+function randomRPS()
+{
 	//Math.random() function to generate a number as computerSelection:
 	//0 - .34 => PAPER,.35 - .67 => SCISSORS, .68 - 1 => ROCK
 	const num = Math.random();
@@ -117,7 +158,8 @@ function randomRPS() {
  * Main function the application will run to play Rock-Paper-Scissors
  */
 //TODO: Chain together all prompts so they're result calls the next apprpriate method mighyt cause some recursion byt eh 🤷
-function main() {
+function main()
+{
 	const playAgainProps = {
 		name: 'Play again,',
 		default: false,
