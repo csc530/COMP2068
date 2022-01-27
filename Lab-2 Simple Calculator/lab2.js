5af898c321e70a7d282eff17f1baee5629a10845
csc530 <cscousin@lakeheadu.ca>
2022-01-27 14:27:21 -0500

    feat: :sparkles: Parse parameters from query string
    Parses each parameter from the url's query string into an object with each property the name of the parameter and its associated value

--- Lab-2 Simple Calculator/lab2.js
+++ Lab-2 Simple Calculator/lab2.js
@@ -8,8 +8,23 @@ function calculate(request, response, next) {
 }
 
 app.use((req, res, next) => {
-    //gets each of the parameters from the url
-	let params = req.url.substring(req.url.indexOf('?') + 1).split('&');
-    console.log(params);
+	//gets each of the parameters from the url
+	let queryString = req.url.substring(req.url.indexOf('?') + 1).split('&');
+	let params = queryString.filter((value) => value.includes('=', 0));
+	const methods =  ["add", "subtract", "multiply", "divide"];
+	let operation = {};
+	for (let i = 0; i < params.length; i++) {
+		//the name will be first element then the value
+		let elems = params[i].split('=');
+		Object.defineProperty(operation, elems[0], {
+			//controls whether or not the property other than the value and writable can be edited or deleted
+			configurable: false,
+			enumerable: true, 
+			value: elems[1],
+			//if value of the property is editable/changeable
+			writable: true
+		});
+	}
+	console.log(operation);
 	res.end();
 });
