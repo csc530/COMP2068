c514e407cc1a4d9b33fc11b5569310de54925a81
csc530 <cscousin@lakeheadu.ca>
2022-01-27 14:00:20 -0500

    feat: :sparkles: parse query string from the url
    Can parse the the parameter name and value from the query string

--- Lab-2 Simple Calculator/lab2.js
+++ Lab-2 Simple Calculator/lab2.js
@@ -4,6 +4,12 @@ let app = connect();
 app.listen(3000);
 
 function calculate(request, response, next) {
+	// will post contents from anonymous function; just easier beacuse ide knows the class of params
 }
 
-app.use(calculate);
+app.use((req, res, next) => {
+    //gets each of the parameters from the url
+	let params = req.url.substring(req.url.indexOf('?') + 1).split('&');
+    console.log(params);
+	res.end();
+});
