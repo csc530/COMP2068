c9a3100c402e568596137d2f05221316b54fc5dc
csc530 <77406318+csc530@users.noreply.github.com>
2022-01-27 18:03:26 -0500

    feat(lab2): :sparkles: calculated result of query string
    Created a new method that will calculate the result of the query string depending on the given method

--- Lab-2 Simple Calculator/lab2.js
+++ Lab-2 Simple Calculator/lab2.js
@@ -3,15 +3,41 @@ const connect = require('connect');
 let app = connect();
 app.listen(3000);
 
-function calculate(request, response, next) {
-	// will post contents from anonymous function; just easier beacuse ide knows the class of params
+function calculate(method, vars, request, response, next) {
+	const result;
+	if (vars.length < 2)
+		next(Error('2 arguments are required\nNot enough arguments to perform valid math operations'));
+	switch (method) {
+		case 'add':
+			for (let i = 0; index < vars.length; i++)
+				result += vars[i];
+			break;
+		case 'subtract':
+			result = vars[0];
+			for (let i = 1; index < vars.length; i++)
+				result += vars[i]; break;
+		case 'multiply':
+			result = vars[0];
+			vars = vars.slice(1);
+			vars.forEach((value) => result *= value);
+			break;
+		case 'divide':
+			result = vars[0];
+			vars = vars.slice(1);
+			vars.forEach((value) => result /= value);
+			break;
+		default:
+			next(new Error('Invalid method'));
+			break;
+			print(result, request, response, next);
+	}
 }
 function parseUrl(req, res, next) {
 	//gets each of the parameters from the url
 	let queryString = req.url.substring(req.url.indexOf('?') + 1).split('&');
 	let params = queryString.filter((value) => value.includes('=', 0));
 	const methods = ['add', 'subtract', 'multiply', 'divide'];
-	let operation = {method: ''};
+	let operation = { method: '' };
 	for (let i = 0; i < params.length; i++) {
 		//the name will be first element then the value
 		let elems = params[i].split('=');
@@ -28,20 +54,15 @@ function parseUrl(req, res, next) {
 	if (!methods.includes(operation.method))
 		next(new Error('Method "' + operation.method + '" does not exist.'));
 	else
-		next();
+		calculate(operation.method, Object.values(operation).slice(1), req, res, next);
 }
 function err(error, req, res, next) {
 	res.writeHead(400, error.message);
-	const msg = '<h1>Error</h1><p>'+error.message+'</p>';
-	console.log('fhdgfhytdr');
-	//res.end();
+	const msg = '<h1>Error</h1><p>' + error.message + '</p>';
+	res.write(msg, 'utf-8');
 	next();
 }
 
 
 app.use(parseUrl);
 app.use(err);
-app.use(function(req, res, next) {
-	res.end('<h1>Welcome</h1>','utf-8');
-	res.end();
-});
