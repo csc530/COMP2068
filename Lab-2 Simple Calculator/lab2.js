918da6e81e9efc027a12bce00d9a3e53a8de64c4
csc530 <cscousin@lakeheadu.ca>
2022-01-27 22:34:26 -0500

    perf(lab2): :zap: Improved switch statement excution
    Factored out common code to run the print method outside the switch statement and suppressed errors from executing next if there was an error. Also added a hom/ welcome page if no parameters are in the url

--- Lab-2 Simple Calculator/lab2.js
+++ Lab-2 Simple Calculator/lab2.js
@@ -6,15 +6,15 @@ const methods =
 		symbol: ' + ',
 	},
 	'subtract': {
-		name: ['subtract', 'minus'],
+		name: ['subtract', 'minus','subtraction'],
 		symbol: ' - ',
 	},
 	'multiply': {
-		name: ['multiply', 'times',],
+		name: ['multiply', 'times','multiplication'],
 		symbol: ' × ',
 	},
 	'divide': {
-		name: ['divide',],
+		name: ['divide','division'],
 		symbol: ' ÷ '
 	},
 	findMethod: function (val) {
@@ -77,49 +77,90 @@ function calculate(operation, request, response, next) {
 	if(terms.length < 2)
 		next(Error('2 arguments are required. Not enough arguments to perform valid math operations'));
 	else
+	{
 		switch(method) {
 		case'add':
 			for(let i = 0; i < terms.length; i++)
 				result += terms[i];
-				//no next function because it's the end of the end of middleware calls
-				//no chances of erros
-			print(result, method, terms, request, response);
 			break;
 		case'subtract':
 			result = terms[0];
 			for(let i = 1; i < terms.length; i++)
 				result -= terms[i];
-				//no next function because it's the end of the end of middleware calls
-				//no chances of erros
-			print(result, method, terms, request, response);
 			break;
 		case'multiply':
 			result = terms[0];
 			terms.slice(1).forEach((value) => result *= value);
-			//no next function because it's the end of the end of middleware calls
-			//no chances of erros
-			print(result, method, terms, request, response);
 
 			break;
 		case'divide':
 			result = terms[0];
 			if(terms.slice(1).includes(0))
-				next(Error('Dividing by zero is not allowed'));
-			else{
+				return next(Error('Dividing by zero is not allowed'));
+			else
 				terms.slice(1).forEach((value) => result /= value);
-				//no next function because it's the end of the end of middleware calls
-				//no chances of erros
-				print(result, method, terms, request, response);
-			}
 			break;
 		default:
-			next(new Error('Invalid method'));
-			break;
+			return next(new Error('Invalid method'));
 		}
+		//no next function because it's the end of the end of middleware calls
+		//no chances of erros
+		print(result, method, terms, request, response);
+	}
+}
+function welcome(req, res, next) {
+	const msg = `
+	<!DOCTYPE html>
+	<html>
+	
+	<head>
+		<meta charset='utf-8'>
+		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+		<title>Simple calculator</title>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<!-- CSS only -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+			integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	</head>
+	
+	<body class="container">
+		<h1 class="display-1">Simple Calculator - webapp</h1>
+		<h2 class="display-2">Instructions</h2>
+		<p class="lead display-6">
+		Type your desired equation in the url above. Using the following syntax:</p>
+		<ol>
+		<li><code><strong>?</strong></code> to start your equation after the website's URL forward slash (${req.headers.host})</li>
+		<li><code><strong>method</strong>: 'math operation'</code></li>
+		<ul>
+			<li>The valid math operations are addtion, multiplication, subtraction, and division.</li>
+			<li>This (argument) is required.</li>
+		</ul>
+		<li><code><strong>&</strong></code>, this is required
+		<li><code>var-name=number&var-different-name=number</code></li>
+		<ul>
+			<li><code>var-name</code>, and subsequent var-name (including <code>var-different-name</code>), is optional and can be left blank.</li>
+			<li>The <code>=</code> is required.</li>
+			<li>the number following the equal sign is also required.</li>
+			<li>No space are allowed within the url.<small>Heavily advised against</small></li>
+			<li>Each number is the value to apply the math operator on in that order. Using the first number as the base value. i.e. numerator for division, initial value for subtraction, etc.</li>
+			<li>You may add as many variables,<code>var-name=number</code>, as you like</li>
+		</ul>
+		</ol>
+		<hr />
+		<pre><p>Example: <code>${req.headers.host + req.url}?method=addition&x=100&=25</code></p></pre>
+		<p>This will add the variable x which is 100 and the unamed variable of value 25 together.</p>
+		<p>All acceptable methods are: ${Object.values(methods).map((val)=>val.name).flat().slice(0,-1).join(', ')}
+	</body>
+	
+	</html>`;
+
+	res.end(msg,'utf-8');
 }
 function parseUrl(req, res, next) {
 	//gets each of the parameters from the url
-	let queryString = req.url.substring(req.url.indexOf('?') + 1).split('&');
+	let queryString = req.url.substring(req.url.indexOf('?') + 1).trim().split('&');
+	if(!req.url.includes('?')||queryString.every((val)=>!!val))
+		return welcome(req, res, next);
 	let params = queryString.filter((value) => value.includes('=', 0));
 	let operation = {method: ''};
 	for(let i = 0; i < params.length; i++) {
@@ -178,3 +219,4 @@ function calculator(req, res, next) {
 }
 app.use(calculator);
 app.use(err);
+//app.use((req, res, next)=>)
