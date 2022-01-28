eabc0960306eda7b04fde8af695a8b042bcdabd3
csc530 <cscousin@lakeheadu.ca>
2022-01-27 21:15:35 -0500

    perf(lab2): :art: Added html page to display errors
    Fixed the error middleware function to include the next parameter (though) unused so it's recognized as the error handling middleware so it can generate user friendly output of their error

--- Lab-2 Simple Calculator/lab2.js
+++ Lab-2 Simple Calculator/lab2.js
@@ -124,13 +124,36 @@ function parseUrl(req, res, next) {
 		calculate(operation, req, res, next);
 }
 
-function err(error, req, res) {
+function err(error, req, res, next) {
 	res.writeHead(400, error.message);
-	const msg = '<h1>Error</h1><p>' + error.message + '</p>';
+	const msg = `
+	<!DOCTYPE html>
+	<html>
+	
+	<head>
+		<meta charset='utf-8'>
+		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+		<title>Simple calculator</title>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<!-- CSS only -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+			integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	</head>
+	
+	<body class="container">
+		<h1 class="display-1">Error⚠</h1>
+		<p class="lead display-5 text-center">${error.message}</p>
+		<hr />
+		<details class="display-6">${error.stack}</details>
+	</body>
+	
+	</html>`;
 	res.write(msg, 'utf8');
 	res.end();
 }
 
-
-app.use(parseUrl);
+function calculator(req, res, next) {
+	parseUrl(req,res,next);
+}
+app.use(calculator);
 app.use(err);
