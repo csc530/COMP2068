f2351333ea4edd1a7e000abba74a1ad7ef32743c
csc530 <cscousin@lakeheadu.ca>
2022-02-04 18:39:48 -0500

    chore(assignment 1 - portfolio): :bricks: Added structure for site
    Added each route and view for the necessary/releveant pages for my website

--- assignment1-portfolio/app.js
+++ assignment1-portfolio/app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+/*----------------------------------------------------------------*/
+//Routers/Controllers
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+let contactRouter = require('./routes/contact');
+let projectsRouter = require('./routes/projects');
 
 var app = express();
 
@@ -20,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
