602db365e1ba3a6a1fce7b252a92f0cc751e02e7
csc530 <cscousin@lakeheadu.ca>
2022-02-05 23:57:11 -0500

    feat(assignment 1 - portfolio): :construction: Implemented js homepage content
    add options for index.hbs

--- assignment1-portfolio/routes/index.js
+++ assignment1-portfolio/routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', {title: 'Express'});
+	const options = {
+		student: {
+			title: 'Student',
+			txt: 'txt',
+		},
+		developer: {
+			title: 'Aspiring Full-Stack Developer',
+			txt: 'txt',
+		},
+		project: {
+			title: 'Projects',
+			txt: 'txt',
+			items: [],
+			img: ''
+		},
+		photo: {
+			title: 'Photo',
+			alt: 'Photo',
+			src: ''
+		},
+	};
+	res.render('index', options);
 });
 
 module.exports = router;
