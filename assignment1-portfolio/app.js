f6713ea434487fe3e537109d75e9f467386a427a
csc530 <cscousin@lakeheadu.ca>
2022-02-07 22:08:55 -0500

    feat(assignment 1 - portfolio): :children_crossing: added website paths
    added the routers for the about, copntact, and projects routes to be functional with their respective controllers and views

--- assignment1-portfolio/app.js
+++ assignment1-portfolio/app.js
@@ -25,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
+app.use('/projects', projectsRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
