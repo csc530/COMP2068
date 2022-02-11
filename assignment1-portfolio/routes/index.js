25e3c44b72dd1319264884be3b901a0089f9b152
csc530 <cscousin@lakeheadu.ca>
2022-02-10 22:40:11 -0500

    chore(assignment 1 - portfolio): :construction: add some content to page
    Added some more content for the index page

--- assignment1-portfolio/routes/index.js
+++ assignment1-portfolio/routes/index.js
@@ -3,25 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+	let stdImg;
+	require('https').get('https://www.lakeheadgeorgian.ca/wp-content/uploads/2018/02/lakehead-georgian-favicon.png', res => {
+		if(res.statusCode === 200 || res.statusCode === 304) stdImg = 'https://www.lakeheadgeorgian.ca/wp-content/uploads/2018/02/lakehead-georgian-favicon.png';
+		else stdImg = 'local/img';
+	});
 	const options = {
 		student: {
 			title: 'Student',
-			txt: 'txt',
+			txt: '@<a href="http://">Lakehead Georgian Honours Bachelor of Science (Computer Science)</a>',
+			img: stdImg,
 		},
 		developer: {
 			title: 'Aspiring Full-Stack Developer',
-			txt: 'txt',
+			txt: '',
+			img: '',
 		},
 		project: {
 			title: 'Projects',
-			txt: 'txt',
-			items: [],
-			img: 'Kaya,_Bane_of_the_Dead.jpg'
+			txt: '',
+			items: [
+				{name: 'Crypto.com Exchange API', link: ''},
+				{name: "Nasa's Astronmy piscture of the Day application", link: ''},
+				{name: '', link: ''},
+			],
+			img: 'images/project.jpg',
 		},
 		photo: {
-			title: 'Photo',
-			alt: 'Photo',
-			src: 'Kaya,_Bane_of_the_Dead.jpg'
+			title: 'A picture of me😃',
+			alt: 'A photo of me',
+			src: '',
 		},
 	};
 	res.render('index', options);
