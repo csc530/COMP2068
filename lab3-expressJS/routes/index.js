7b5074304b0b4ad4e20d72228155014ac21c9b8e
csc530 <cscousin@lakeheadu.ca>
2022-02-20 17:26:41 -0500

    feat(lab3): :lipstick: Add home page
    created a home page for the (essentially) family tree website

--- lab3-expressJS/routes/index.js
+++ lab3-expressJS/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const options = {
+	title: 'A website for my family',
+	msg: 'A small page with a brief description about me and my family.',
+};
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', {title: 'Express'});
+router.get('/', function (req, res, next) {
+	res.render('index', options``);
 });
 
 module.exports = router;
