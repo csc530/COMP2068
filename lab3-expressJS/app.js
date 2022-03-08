bdf3a838f10c8698de3fa972f18d1602939f631d
csc530 <cscousin@lakeheadu.ca>
2022-03-08 13:20:18 -0500

    feat(lab3): :construction: Add routers for each family member in app.js

--- lab3-expressJS/app.js
+++ lab3-expressJS/app.js
@@ -4,8 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// ? controllers
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var momRouter = require('./routes/christene');
+const dadRouter = require('./routes/marc');
+const meRouter = require('./routes/chris');
+const sis1Router = require('./routes/lydea');
+const sis2Router = require('./routes/samantha');
+
 
 var app = express();
 
@@ -15,27 +21,33 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// ? add routers
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/Christene', momRouter);
+app.use('/Marc', dadRouter);
+app.use('/Christofer', meRouter);
+app.use('/Lydea', sis1Router);
+app.use('/Samantha', sis2Router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
