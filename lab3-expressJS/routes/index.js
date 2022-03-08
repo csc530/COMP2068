2259b3fd6917be6b1f74203437561bbfb8aab2b1
csc530 <cscousin@lakeheadu.ca>
2022-03-08 13:13:33 -0500

    feat(lab3): :tada: Recreate lab 3 express template using EJS view engine

--- lab3-expressJS/routes/index.js
+++ lab3-expressJS/routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const options = {
-	title: 'A website for my family',
-	msg: 'A small page with a brief description about me and my family.',
-};
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('index', options``);
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
