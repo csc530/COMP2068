56f97a8670a27d156b6a1f9a0b354062809338d9
csc530 <cscousin@lakeheadu.ca>
2022-03-09 11:43:26 -0500

    feat(lab3): :speech_balloon: Update titles in routers of my family

--- lab3-expressJS/routes/index.js
+++ lab3-expressJS/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', {title: 'My family'});
+	res.render('index', {title: 'Lab3 - My family'});
 });
 
 module.exports = router;
