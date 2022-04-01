cf0f9b1245e7fc481d61d805b40cb2b12e4424b6
csc530 <cscousin@lakeheadu.ca>
2022-04-01 08:53:49 -0400

    feat(assignment2-job-apps): :sparkles: Create add application page
    Added router logic to handle navigating to the page and getting the POST results from it. Also added some style to align my select nicely.
    
    Currently no authentication so userId is always null

--- assingment2/express/routes/application.js
+++ assingment2/express/routes/application.js
@@ -1,5 +1,8 @@
+
 var express = require('express');
 var router = express.Router();
+
+const User = require('./../models/user');
 const Application = require('./../models/application');
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -10,8 +13,42 @@ router.get('/', function (req, res, next) {
 		if(err)
 			return console.log(err);
 		renderParams.data = applications;
-		res.render('application', renderParams);
+		res.render('application/index', renderParams);
 	});
 });
 
+/* GET add application */
+router.get('/add', (req, res, next)=>{
+	const renderParams = {
+		title: 'Create new application'
+	};
+	res.render('application/add', renderParams);
+});
+/* POST add application */
+router.post('/add',(req, res, next)=>{
+	const uid = req.sessionID;//TODO replace with req.user.id
+	const values = req.body;
+	const jobTitle = values.jobTitle.toString().trim();
+	console.log(jobTitle);
+	const postedDate = values.postedDate;
+	const action = values.action;
+	const applicationDate = values.applicationDate;
+	Application.create({
+		jobTitle: jobTitle,
+		applicationDate: applicationDate,
+		postedDate: postedDate,
+		action: action,
+		uid: uid
+	}, (err, application)=>{
+		if(err)
+		{
+			console.log(`Error: ${err}`);
+			res.status(400);
+			//redirect back with status code 400 displaying error
+			res.redirect('add');
+		}
+		else
+			res.redirect('add');
+	});
+});
 module.exports = router;
