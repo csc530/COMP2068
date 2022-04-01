c7d099577ed599e6c9c5c82dd55ad84fc14425ec
csc530 <cscousin@lakeheadu.ca>
2022-04-01 09:00:51 -0400

    revert(assignment2-job-apps): :ambulance: restore project files

---
+++ assingment2/app/models/application.js
@@ -0,0 +1,24 @@
+// Import mongoose
+const mongoose = require('mongoose');
+
+// Expanded definition
+const schemaDefinition = {
+	jobTitle: {
+		type: String,
+		required: true,
+	},
+	applicationDate: Date,
+	action: {
+		type: String,
+		default: 'TO DO'
+	},
+	postedDate: Date,
+	uid: {
+		type: String,
+		required: true
+	}
+};
+// Create a new mongoose schema using the definition object
+var mongooseSchema = new mongoose.Schema(schemaDefinition);
+// Create a new mongoose model using the mongoose schema and export the new model
+module.exports = mongoose.model('Project', mongooseSchema);
