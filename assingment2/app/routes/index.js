3d7569c8f27e2502eb31d591d06006cad223a853
csc530 <cscousin@lakeheadu.ca>
2022-04-01 10:42:50 -0400

    feat(assignment2-job-apps): :children_crossing: Add content to start page
    add some content to the footer aswell

--- assingment2/app/routes/index.js
+++ assingment2/app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', {title: 'Express'});
+	res.render('index', {title: 'Job application Manager'});
 });
 
 module.exports = router;
